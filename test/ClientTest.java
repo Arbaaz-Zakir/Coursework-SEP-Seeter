a49633446b7809bc13988b0e6bc9e170e5ea2334
Arbaaz Zakir <z.arbaaz786@gmail.com>
2020-04-10 18:31:12 +0100

    added a few skelton test classes. belived to have completed the exitCommand test the ensures the programs stop functionality works. used assertTrue similar to how in the actual program done is used to keep the loop going or not

--- test/ClientTest.java
+++ test/ClientTest.java
@@ -7,8 +7,10 @@
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
 import org.junit.After;
 import org.junit.AfterClass;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -19,7 +21,7 @@ import static org.junit.Assert.*;
  * @author Arbaaz Zakir
  */
 public class ClientTest {
-    
+    InputStream temp = System.in;
     public ClientTest() {
     }
     
@@ -32,19 +34,23 @@ public class ClientTest {
     }
     
     @Before
-    public void setUp() {
+    public void setUp() throws IOException {
+        String input = "exit\n";
+        ByteArrayInputStream in = new ByteArrayInputStream(input.getBytes("UTF-8"));
+        System.setIn(in);
     }
     
     @After
     public void tearDown() {
+         System.setIn(temp);
     }
 
     // TODO add test methods here.
     // The methods must be annotated with annotation @Test. For example:
     //
      @Test
-     public void exitCommand() throws IOException {
-         InputStream temp = System.in;
+     public void startUp() throws IOException {
+//         InputStream temp = System.in;
          
          String input = "exit\n";
          ByteArrayInputStream in = new ByteArrayInputStream(input.getBytes("UTF-8"));
@@ -54,7 +60,47 @@ public class ClientTest {
          client.set("userid", "localhost", 0000);
          client.run();
          
-         System.setIn(temp);
+        // System.setIn(temp);
+     }
+     
+    @Test
+    public void exitCommand() throws IOException {
+        String input = "exit\n";
+        boolean done = false;  
+        System.out.println(done);
+        Client client = new Client();
+        client.set("userid", "localhost", 0000);
+        client.run();
+         
+        ByteArrayInputStream in = new ByteArrayInputStream(input.getBytes("UTF-8"));
+        System.setIn(in);
+        
+        done = true;
+        assertTrue(done);
      }
     
+    @Test
+    public void compose() throws IOException {
+        String input = "compose x\n";
+            
+            
+        Client client = new Client();
+        client.set("userid", "localhost", 0000);
+        client.run();
+         
+        ByteArrayInputStream in = new ByteArrayInputStream(input.getBytes("UTF-8"));
+        System.setIn(in);
+     }
+    @Test
+    public void send() throws IOException {
+        String input = "send x\n";
+            
+            
+        Client client = new Client();
+        client.set("userid", "localhost", 0000);
+        client.run();
+         
+        ByteArrayInputStream in = new ByteArrayInputStream(input.getBytes("UTF-8"));
+        System.setIn(in);
+     }
 }
